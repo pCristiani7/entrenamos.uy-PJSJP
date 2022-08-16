5471ef404ac79d88a1852f160867b18912b3ab36
LordAmbush <PedroAldama47@gmail.com>
2022-08-16 11:00:14 -0300

    Clases creadas

---
+++ main/java/logica/Profesor.java
@@ -0,0 +1,77 @@
+package logica;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Profesor extends Usuario {
+	private String descripcion;
+	private String biografia;
+	private String sitioWeb;
+	private List<Clase> clases = new ArrayList<>();
+	private InstitucionDeportiva institucionDeportiva;
+	
+	
+	public Profesor() {
+		super();
+	}
+
+	public Profesor(String descripcion, String biografia, String sitioWeb, List<Clase> clases,
+			InstitucionDeportiva institucionDeportiva) {
+		super();
+		this.descripcion = descripcion;
+		this.biografia = biografia;
+		this.sitioWeb = sitioWeb;
+		this.clases = clases;
+		this.institucionDeportiva = institucionDeportiva;
+	}
+
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getBiografia() {
+		return biografia;
+	}
+
+	public void setBiografia(String biografia) {
+		this.biografia = biografia;
+	}
+
+	public String getSitioWeb() {
+		return sitioWeb;
+	}
+
+	public void setSitioWeb(String sitioWeb) {
+		this.sitioWeb = sitioWeb;
+	}
+
+	public List<Clase> getClases() {
+		return clases;
+	}
+
+	public void setClases(List<Clase> clases) {
+		this.clases = clases;
+	}
+
+	public InstitucionDeportiva getInstitucionDeportiva() {
+		return institucionDeportiva;
+	}
+
+	public void setInstitucionDeportiva(InstitucionDeportiva institucionDeportiva) {
+		this.institucionDeportiva = institucionDeportiva;
+	}
+	
+	
+
+	
+	
+	
+	
+	
+}
