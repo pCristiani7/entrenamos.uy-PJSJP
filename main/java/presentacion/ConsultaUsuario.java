1fca574a291ba2bffbcae4a2691c86cd6a7e9604
pCristiani7 <pact1602@gmail.com>
2022-09-09 22:22:37 -0300

    Se arreglo pequeños errores en el front de, consultaActividadDeportiva, consultaDictadoDeClase, consultaUsuario y RegistroAClase ;D

--- main/java/presentacion/ConsultaUsuario.java
+++ main/java/presentacion/ConsultaUsuario.java
@@ -21,6 +21,8 @@ import interfaces.IControlador;
 
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
@@ -96,6 +98,15 @@ public class ConsultaUsuario extends JInternalFrame {
 		comboBoxUsuarios = new JComboBox<String>();
 		comboBoxUsuarios.setFont(new Font("Dialog", Font.PLAIN, 12));
 		comboBoxUsuarios.setBounds(20, 138, 229, 36);
+		comboBoxUsuarios.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if(e.getStateChange() == ItemEvent.SELECTED) {
+					if(!comboBoxUsuarios.getSelectedItem().toString().equals(e)) {
+						limpiarScreenOnClosing();
+					}
+				}
+			}
+		});
 		getContentPane().add(comboBoxUsuarios);
 
 		lblNewLabelNickname = new JLabel("Nickname");
@@ -185,12 +196,30 @@ public class ConsultaUsuario extends JInternalFrame {
 		comboBoxClasesDictadas.setFont(new Font("Dialog", Font.PLAIN, 12));
 		comboBoxClasesDictadas.setBounds(289, 340, 174, 41);
 		comboBoxClasesDictadas.setVisible(false);
+		comboBoxClasesDictadas.addItemListener(new ItemListener() {
+		public void itemStateChanged(ItemEvent e) {
+			if(e.getStateChange() == ItemEvent.SELECTED) {
+				if(!comboBoxClasesDictadas.getSelectedItem().toString().equals(e)) {
+					clearPane();
+				}
+			}
+		}
+		});
 		getContentPane().add(comboBoxClasesDictadas);
 		
 		comboBoxRegistroClases = new JComboBox<String>();
 		comboBoxRegistroClases.setFont(new Font("Dialog", Font.PLAIN, 15));
 		comboBoxRegistroClases.setBounds(511, 340, 174, 41);
 		comboBoxRegistroClases.setVisible(false);
+		comboBoxRegistroClases.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if(e.getStateChange() == ItemEvent.SELECTED) {
+					if(!comboBoxRegistroClases.getSelectedItem().toString().equals(e)) {
+						clearPane();
+					}
+				}
+			}
+		});
 		getContentPane().add(comboBoxRegistroClases);
 		
 		btnConsultarDependsRegistro = new JButton("Consultar");
@@ -356,9 +385,14 @@ public class ConsultaUsuario extends JInternalFrame {
 	public boolean inicializarComboBoxRegistros() {
 		String s = comboBoxUsuarios.getSelectedItem().toString();
 		DefaultComboBoxModel<String> modelUsr = new DefaultComboBoxModel<String>(iCon.listarRegistrosSocio(s));
-		if(modelUsr.getSize() == 0)
-			return false;
-		else {
+		if(modelUsr.getSize() == 0) {
+			comboBoxRegistroClases.setSelectedIndex(-1);
+		comboBoxRegistroClases.setEnabled(false);
+		this.btnConsultarDependsRegistro.setEnabled(false);
+		return false;
+		}else {
+			comboBoxRegistroClases.setEnabled(true);
+			this.btnConsultarDependsRegistro.setEnabled(true);
 			comboBoxRegistroClases.setModel(modelUsr);
 			return true;
 		}
@@ -394,11 +428,21 @@ public class ConsultaUsuario extends JInternalFrame {
 	public boolean inicializarComboBoxClasesDictadas() {
 		String p = comboBoxUsuarios.getSelectedItem().toString();
 		DefaultComboBoxModel<String> modelUsr = new DefaultComboBoxModel<String>(iCon.listarClasesProf(p));
-		if(modelUsr.getSize() == 0)
+		if(modelUsr.getSize() == 0) {
+			comboBoxClasesDictadas.setSelectedIndex(-1);
+			comboBoxClasesDictadas.setEnabled(false);
+			this.btnConsultarDependsRegistro.setEnabled(false);
 			return false;
-		else {
+		}else {
+			comboBoxClasesDictadas.setEnabled(true);
+			this.btnConsultarDependsDictado.setEnabled(true);
 			comboBoxClasesDictadas.setModel(modelUsr);
 			return true;
 		}
+	}
+	
+	public void clearPane() {
+		this.textPaneDataProfesor.setVisible(false);
+		this.textPaneDataSocio.setVisible(false);
 	}
 }
