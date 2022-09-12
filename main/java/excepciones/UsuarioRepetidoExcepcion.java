7627e4548a012d358902cd335c3ba8ebe015adec
Pablo Cristiani <pact1602@gmail.com>
2022-09-12 20:39:38 -0300

    tarea 1
    Proyecto tarea 1

--- main/java/excepciones/UsuarioRepetidoExcepcion.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-public class UsuarioRepetidoExcepcion extends Exception{
-	private static final long serialVersionUID = 1L;
-
-	public UsuarioRepetidoExcepcion(String string) {
-        super(string);
-    }
-}
